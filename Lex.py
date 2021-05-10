4c46334883f5d0ed85dc4c5cea4b17c67dfbbe3b
Nicolás Calvache Jiménez <nicolas.calvache@alumnos.uneatlantico.es>
2021-05-10 18:11:15 +0200

    changed structure

--- Lex.py
+++ Lex.py
@@ -1,4 +1,30 @@
-import LexData
+statesTable = [
+    # q, T, ;, a1, 1, =, >, <, !
+    [0, 0, 1, 2, 3, 4, 5, 6, 7],
+    [1, 1, -1, -1, -1, -1, -1, -1, -1],
+    [2, 1, -1,  2, 2, -1, -1, -1, -1],
+    [3, 1, -1, -1, 3, -1, -1, -1, -1],
+    [4, 1, -1, -1, -1, 8, -1, -1, -1],
+    [5, 1, -1, -1, -1, 9, -1, -1, -1],
+    [6, 1, -1, -1, -1, 10, -1, -1, -1],
+    [7, 0, -1, -1, -1, 11, -1, -1, -1],
+    [8, 1, -1, -1, -1, -1, -1, -1, -1],
+    [9, 1, -1, -1, -1, -1, -1, -1, -1],
+    [10, 1, -1, -1, -1, -1, -1, -1, -1],
+    [11, 1, -1, -1, -1, -1, -1, -1, -1],
+]
+
+identifiers = ["<TERM>",
+               "<var>",
+               "<num>",
+               "<eqTo>",
+               "<GreatT>",
+               "<LessT",
+               " ",
+               "<EqualT>",
+               "<GreEqT>",
+               "<LesEqT>",
+               "<DiffT>"]
 
 
 class Lex:
@@ -39,33 +65,27 @@ class Lex:
         stringLen = len(self.value)
         currentState = 0
         id = ""
+
         for i in range(0, stringLen):
-            currentState = LexData.statesTable[currentState][self.inputColumns[i]]
+            currentState = statesTable[currentState][self.inputColumns[i]]
             id += self.value[i]
 
             if i == stringLen - 1:
-                if LexData.statesTable[currentState][1] == 1:
+                if statesTable[currentState][1] == 1:
                     self.res.append(id)
                     id = ""
 
-                    self.ident.append(LexData.identifiers[currentState - 1])
+                    self.ident.append(identifiers[currentState - 1])
                     currentState = 0
 
                 else:
                     raise TypeError("\n\nInvalid symbol: '" +
                                     self.value[i] + "'")
             else:
-                if LexData.statesTable[currentState][1] == 1 and (LexData.statesTable[currentState][self.inputColumns[i + 1]] < 0):
+                if statesTable[currentState][1] == 1 and (statesTable[currentState][self.inputColumns[i + 1]] < 0):
                     self.res.append(id)
                     id = ""
 
-                    self.ident.append(LexData.identifiers[currentState - 1])
+                    self.ident.append(identifiers[currentState - 1])
                     currentState = 0
         return self
-
-
-try:
-    lexer = Lex(input("Please input string to check: ")).iterateString().iterateStates()
-    print(lexer.getResult(), lexer.getIdent())
-except TypeError as err:
-    print(err)
